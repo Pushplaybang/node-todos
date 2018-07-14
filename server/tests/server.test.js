a086569f9039ef788dcddd5624b7f627fe795f1c
pushplaybang <pushplaybang@gmail.com>
2018-07-14 17:38:43 +0200

    update tests for first GET route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,26 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../db/models/Todo');
 
 /* global beforeEach describe it */
+const initTodos = [
+  {
+    _id: new ObjectID(),
+    text: 'First test todo',
+  },
+  {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+  },
+];
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => Todo.insertMany(initTodos))
+    .then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +39,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find()
+        Todo.find({ text: 'test todo text' })
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -45,12 +58,35 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
+
         Todo.find()
           .then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(e => done(e));
       });
   });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => expect(res.body.todos.length).toBe(2))
+      .end(done);
+  });
+});
+
+describe('GET todos/:id', () => {
+  it('should get a specific todo by id');
+});
+
+describe('PUT todos/:id', () => {
+  it('should update a specific todo by id');
+});
+
+describe('DELETE todos/:id', () => {
+  it('should delete a specific todo by id');
 });
